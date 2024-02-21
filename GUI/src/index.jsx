b6c3ee09e9b7259ecb26b8dcbb3edc9de9e91557
wyq <489264018@qq.com>
2024-02-22 01:56:43 +0800

    feat: add debug func

--- GUI/src/index.jsx
+++ GUI/src/index.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
+import { debugToWindow } from './utils/methods';
 import { store } from './store/store';
 import App from './App';
-
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
 );
+
+debugToWindow('store', store); // 用于调试redux store
 
 root.render(
   <React.StrictMode>
